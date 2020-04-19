25912f3bba9dfcff9178d1bd234a0e553e18eab6
Amanda Hagy <amandahagy@hotmail.com>
2020-04-19 01:34:59 -0300

    Adding Models

---
+++ SmartBirthWebAPI/SmartBirthWebAPI/Models/BirthRegistration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using SmartBirthWebAPI.Models;
+
+namespace SmartBirthWebAPI.Models
+{
+    public class BirthRegistration
+    {
+        public int BirthRegistrationCode { get; set; }
+        public string NewbornName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public int Parent1Code { get; set; }
+        public int Parent2Code { get; set; }
+
+        public User Parent1 { get; set; }
+        public User Parent2 { get; set; }
+
+        public BirthRegistration()
+        {
+        }
+
+        public BirthRegistration(int BirthRegistrationCode, string NewbornName, DateTime BirthDate, int Parent1Code, int Parent2Code)
+        {
+            this.BirthRegistrationCode = BirthRegistrationCode;
+            this.NewbornName = NewbornName;
+            this.BirthDate = BirthDate;
+            this.Parent1Code = Parent1Code;
+            this.Parent2Code = Parent2Code;
+        }
+    }
+}
---
+++ SmartBirthWebAPI/SmartBirthWebAPI/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmartBirthWebAPI.Models
+{
+    public class User
+    {
+        public int UserCode { get; set; }
+        public string Name { get; set; }
+        public long Rg { get; set; }
+        public long Cpf { get; set; }
+        public DateTime Birth { get; set; }
+
+        public List<BirthRegistration> Heirs { get; set; }
+
+        public User()
+        {
+            this.Heirs = new List<BirthRegistration>();
+        }
+
+        public void AddHeir(BirthRegistration birthRegistration)
+		{
+            this.Heirs.Add(birthRegistration);
+		}
+
+        public void Delete(long id)
+		{
+            BirthRegistration birthRegistration = Heirs.FirstOrDefault(p => p.BirthRegistrationCode == id);
+            Heirs.Remove(birthRegistration);
+		}
+
+        public void Update(BirthRegistration birthRegistration)
+		{
+            Delete(birthRegistration.BirthRegistrationCode);
+            AddHeir(birthRegistration);
+		}
+    }
+}
