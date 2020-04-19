f47bcc3bca4308c8de444f8e4b4a3d2ef635b111
Amanda Hagy <amandahagy@hotmail.com>
2020-04-19 02:20:40 -0300

    Adding controller and references

--- SmartBirthWebAPI/SmartBirthWebAPI/App_Start/WebApiConfig.cs
+++ SmartBirthWebAPI/SmartBirthWebAPI/App_Start/WebApiConfig.cs
@@ -1,4 +1,7 @@
 ﻿using System.Web.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace SmartBirthWebAPI
 {
---
+++ SmartBirthWebAPI/SmartBirthWebAPI/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using SmartBirthWebAPI.Models;
+using SmartBirthWebAPI.DAL;
+using System.Collections.Generic;
+
+namespace SmartBirthWebAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                UserDAL dal = new UserDAL();
+                User User = dal.Find(id);
+                return Ok(User);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
