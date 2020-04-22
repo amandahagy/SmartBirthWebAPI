13313478e5bedc2b3608d72aa2007d6d337f17fb
Amanda Hagy <amandahagy@hotmail.com>
2020-04-21 23:54:07 -0300

    BirthRegistration

---
+++ SmartBirthWebAPI/SmartBirthWebAPI/Controllers/BirthRegistrationController.cs
@@ -0,0 +1,77 @@
+﻿using System.Web.Http;
+using SmartBirthWebAPI.DAL;
+using SmartBirthWebAPI.Models;
+using System.Collections.Generic;
+using System;
+
+namespace SmartBirthWebAPI.Controllers
+{
+    public class BirthRegistrationController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(new BirthRegistrationDAL().List());
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                BirthRegistrationDAL dal = new BirthRegistrationDAL();
+                BirthRegistration BirthRegistration = dal.Find(id);
+                return Ok(BirthRegistration);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        public IHttpActionResult Post([FromBody] BirthRegistration BirthRegistration)
+        {
+            try
+            {
+                BirthRegistrationDAL dal = new BirthRegistrationDAL();
+                dal.Insert(BirthRegistration);
+
+                string location =
+                    Url.Link("DefaultApi",
+                    new { controller = "birthRegistration", id = BirthRegistration.BirthRegistrationCode });
+
+                return Created(new Uri(location), BirthRegistration);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                BirthRegistrationDAL dal = new BirthRegistrationDAL();
+                dal.Delete(id);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        public IHttpActionResult Put([FromBody] BirthRegistration birthRegistration)
+        {
+            try
+            {
+                BirthRegistrationDAL dal = new BirthRegistrationDAL();
+                dal.Update(birthRegistration);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ SmartBirthWebAPI/SmartBirthWebAPI/DAL/BirthRegistrationDAL.cs
@@ -0,0 +1,52 @@
+﻿using SmartBirthWebAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SmartBirthWebAPI.DAL
+{
+    public class BirthRegistrationDAL
+    {
+        private static Dictionary<long, BirthRegistration> databaseBirthRegistration = new Dictionary<long, BirthRegistration>();
+        private static int databaseCounter = 2;
+
+        static BirthRegistrationDAL()
+        {
+            BirthRegistration Martin = new BirthRegistration();
+            Martin.BirthRegistrationCode = 400;
+            Martin.NewbornName = "Martin Watson";
+            var date10 = new DateTime(2020, 1, 1, 10, 40, 00);
+            Martin.BirthDate = date10;
+            Martin.Parent1Code = 1;
+            Martin.Parent2Code = 3;
+
+            databaseBirthRegistration.Add(1, Martin);
+        }
+
+        public void Insert(BirthRegistration BirthRegistration)
+        {
+            databaseCounter++;
+            BirthRegistration.BirthRegistrationCode = databaseCounter;
+            databaseBirthRegistration.Add(databaseCounter, BirthRegistration);
+        }
+
+        public BirthRegistration Find(int BirthRegistrationCode)
+        {
+            return databaseBirthRegistration[BirthRegistrationCode];
+        }
+
+        public IList<BirthRegistration> List()
+        {
+            return new List<BirthRegistration>(databaseBirthRegistration.Values);
+        }
+
+        public void Delete(int BirthRegistrationCode)
+        {
+            databaseBirthRegistration.Remove(BirthRegistrationCode);
+        }
+
+        public void Update(BirthRegistration birthRegistration)
+        {
+            databaseBirthRegistration[birthRegistration.BirthRegistrationCode] = birthRegistration;
+        }
+    }
+}
